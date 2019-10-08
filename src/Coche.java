1180437e89ce0260e445d63ef15c105714cc304f
a19martaff <a19martaff@192.168.56.1>
2019-10-08 17:54:31 +0200

    brum,brum

--- src/Coche.java
+++ src/Coche.java
@@ -3,7 +3,7 @@ public class Coche {
 
 	public Coche() {
 		// TODO Apéndice de constructor generado automáticamente
-	System.out.println("Soy un coche,brum");
+	System.out.println("Soy un coche,brum,brum");
 	
 	
 	}
